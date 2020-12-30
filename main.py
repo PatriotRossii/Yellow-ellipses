f59b9f70ecdaf52e69fa210a13d35ed6acfb8b86
PatriotRossii <patriotrossii2019@mail.ru>
2020-12-30 14:14:34 +0500

    Draw random color ellipsis instead of yellow

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
+
+
+class Interface:
+    def __init__(self, parent):
+        main_layout = QVBoxLayout()
+
+        self.pushButton = QPushButton("Рисовать случайные окружности")
+        main_layout.addWidget(self.pushButton)
+
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        parent.setCentralWidget(main_widget)
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+
+        self.resize(500, 500)
+        self.interface = Interface(self)
 
         self.timer = QTimer()
         self.ellipses = []
@@ -19,7 +33,7 @@ class Example(QMainWindow):
 
     def init_signals(self):
         self.timer.timeout.connect(self.add_ellipse)
-        self.pushButton.clicked.connect(self.start_timer)
+        self.interface.pushButton.clicked.connect(self.start_timer)
 
     def start_timer(self):
         self.timer.start(500)
@@ -36,17 +50,18 @@ class Example(QMainWindow):
     def add_ellipse(self):
         self.ellipses.append(
             (
-                random.randint(0, self.width()),
-                random.randint(0, self.height()),
-                random.randint(0, 250),
-                random.randint(0, 250),
+                [random.randint(0, 255) for _ in range(3)],
+                (random.randint(0, self.width()),
+                 random.randint(0, self.height()),
+                 random.randint(0, 250),
+                 random.randint(0, 250))
             )
         )
         self.repaint()
 
     def draw_ellipse(self, qp, ellipse):
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(*ellipse)
+        qp.setBrush(QColor(*ellipse[0]))
+        qp.drawEllipse(*ellipse[1])
 
 
 if __name__ == '__main__':
